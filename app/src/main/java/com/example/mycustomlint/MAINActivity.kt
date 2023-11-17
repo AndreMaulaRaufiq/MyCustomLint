08a21d42ce0fb3c2cb945db971469a473aa4ac48
Andre Maula R <andremr107@gmail.com>
2023-11-17 22:13:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mycustomlint/MAINActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mycustomlint
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MAINActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
